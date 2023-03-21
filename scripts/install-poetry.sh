31c73bfea5720bcecb21088be1e43a2d7e2c3ed8
Josep Oriol Ayats <oriol.ayats@gmail.com>
2023-03-21 18:18:34 +0100

    Update install-poetry.sh (#3)
    The poetry install link doesn't work anymore. I'm suggesting to replace it with the newer one, gotten from python-poetry.org/docs

--- scripts/install-poetry.sh
+++ scripts/install-poetry.sh
@@ -5,5 +5,5 @@ set -e
   PIP_REQUIRE_VIRTUALENV="1" pip install -U poetry
 } || {
   # For those who dont' use venv's in their workflow
-  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+  curl -sSL https://install.python-poetry.org | python3 -
 }
